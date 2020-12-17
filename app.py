c721c3bdc92519a3e98e96394616f788c521cfc4
KiranRaghavendra1248 <kiran.raghu.1248@gmail.com>
2020-12-17 09:03:02 +0530

    Added update package cost

--- app.py
+++ app.py
@@ -9,8 +9,13 @@ app.config['MYSQL_HOST']=db['mysql_host']
 app.config['MYSQL_USER']=db['mysql_user']
 app.config['MYSQL_PASSWORD']=db['mysql_password']
 app.config['MYSQL_DB']=db['mysql_db']
-
+user_table_name= db['user_table_name']
+package_table_name= db['package_table_name']
+enquiry_table_name= db['enquiry_table_name']
+employee_table_name= db['employee_table_name']
+booking_table_name= db['booking_table_name']
 mysql= MySQL(app)
+
 
 # Webpages for different functions:
 #     1.View all packages: http://localhost:5000/all_packages
@@ -22,6 +27,7 @@ mysql= MySQL(app)
 #     7.View all employees: http://localhost:5000/all_employees
 #     8.All employee: http://localhost:5000/add_employee
 #     9.Remove employee: http://localhost:5000/rem_employee
+#     10. Update package cost: http://localhost:5000/update_pack
 
 
 
@@ -42,7 +48,7 @@ def add_package():
         # Create cursor object to execute commands on MYSQL Server
         cur = mysql.connection.cursor()
         try:
-            cur.execute("INSERT INTO packages values(%s,%s,%s,%s,%s,%s)",(p_id,p_name,days,destination,e_id,price))
+            cur.execute("INSERT INTO "+package_table_name+" values(%s,%s,%s,%s,%s,%s)",(p_id,p_name,days,destination,e_id,price))
         except:
             return 'Package ID already exists'
         mysql.connection.commit()
@@ -60,17 +66,37 @@ def remove_package():
 
         # Create cursor object to execute commands on MYSQL Server
         cur = mysql.connection.cursor()
-        cur.execute("Delete from packages where p_id=(%s)",[p_id_])
+        cur.execute("Delete from "+package_table_name+" where p_id=(%s)",[p_id_])
         mysql.connection.commit()
         cur.close()
         return redirect('/all_packages')
     return render_template('Remove_package.html')
 
+# Update package cost
+@app.route('/update_pack',methods=['GET','POST'])
+def update_pack():
+    if request.method=='POST':
+        # Fetch Form Data
+        package_= request.form
+        p_id_=int(package_["p_id"])
+        price=int(package_["price"])
+
+        # Create cursor object to execute commands on MYSQL Server
+        cur = mysql.connection.cursor()
+
+        cur.execute("Update "+package_table_name+"\nset cost={0}\nwhere p_id={1}".format(price,p_id_))
+        mysql.connection.commit()
+        cur.close()
+        return redirect('/all_packages')
+    return render_template('Update_package.html')
+
+
+
 # View all packages
 @app.route('/all_packages')
 def packages():
     cur = mysql.connection.cursor()
-    result=cur.execute("SELECT * FROM packages")
+    result=cur.execute("SELECT * FROM "+package_table_name)
     if result>0:
         package_details=cur.fetchall()
         return render_template('packages.html',package_details=package_details)
@@ -79,7 +105,7 @@ def packages():
 @app.route('/all_users')
 def users():
     cur = mysql.connection.cursor()
-    result=cur.execute("SELECT * FROM user")
+    result=cur.execute("SELECT * FROM "+user_table_name)
     if result>0:
         user_details=cur.fetchall()
         return render_template('users.html',user_details=user_details)
@@ -88,7 +114,7 @@ def users():
 @app.route('/all_employees')
 def employees():
     cur = mysql.connection.cursor()
-    result=cur.execute("SELECT * FROM employee")
+    result=cur.execute("SELECT * FROM "+employee_table_name)
     if result>0:
         employee_details=cur.fetchall()
         return render_template('employees.html',employee_details=employee_details)
@@ -108,7 +134,7 @@ def add_employee():
         # Create cursor object to execute commands on MYSQL Server
         cur = mysql.connection.cursor()
         try:
-            cur.execute("INSERT INTO employee values(%s,%s,%s,%s)",(e_id,e_name,dept,role))
+            cur.execute("INSERT INTO "+employee_table_name+" values(%s,%s,%s,%s)",(e_id,e_name,dept,role))
         except :
             return 'This Employee ID already exists'
         mysql.connection.commit()
@@ -126,7 +152,7 @@ def remove_employee():
 
         # Create cursor object to execute commands on MYSQL Server
         cur = mysql.connection.cursor()
-        cur.execute("Delete from employee where e_id=(%s)",[e_id_])
+        cur.execute("Delete from "+employee_table_name+" where e_id=(%s)",[e_id_])
         mysql.connection.commit()
         cur.close()
         return redirect('/all_employees')
@@ -136,7 +162,7 @@ def remove_employee():
 @app.route('/all_bookings')
 def bookings():
     cur = mysql.connection.cursor()
-    result=cur.execute("SELECT * FROM bookings")
+    result=cur.execute("SELECT * FROM "+booking_table_name)
     if result>0:
         booking_details=cur.fetchall()
         return render_template('bookings.html',booking_details=booking_details)
@@ -145,7 +171,7 @@ def bookings():
 @app.route('/all_enquiries')
 def enquiry():
     cur = mysql.connection.cursor()
-    result=cur.execute("SELECT * FROM enquiry")
+    result=cur.execute("SELECT * FROM "+enquiry_table_name)
     if result>0:
         enquiry_details=cur.fetchall()
         return render_template('enquiries.html',enquiry_details=enquiry_details)
